47e70d55c5222678633a7a7f153e62bdee5acd67
Islomjon-2013 <hulkarsiddiqova747@gmail.com>
2022-10-03 12:04:35 -0400

    Java script practisec

---
+++ main.js
@@ -0,0 +1,71 @@
+let name1 = "Ibrohim";
+console.log(name1);
+//data types
+let firstName = "hulkaroy"; //string
+age = 31; //number
+isMarried = true; //boolean
+console.log(age);
+//objects
+let fruits = {
+  dry: "nut",
+  wet: "apple",
+};
+console.log(fruits.dry);
+//arrays
+colors = ["green", "white", "red"];
+colors[3] = ["moviy"];
+console.log(colors[3]);
+
+//function
+function greet(name2, lastName) {
+  console.log("hello," + name2 + lastName);
+}
+greet("Hulkar", "Siddikova");
+
+function square(number) {
+  return number * number;
+}
+let squareResult = square(2);
+console.log(squareResult);
+
+//operators
+//arifmetik;
+/*let x = 2;
+y = 3;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+console.log(x++);
+console.log(--x);
+Assignment operators
+x += 3;// x = x + 3
+console.log(x);*/
+
+//Comparison Operators
+
+let x = 5;
+//console.log(x >= 1);
+console.log(x === 1);
+console.log(x !== 1);
+//ternary operators
+let points = 70;
+let result = points > 60 ? "passed" : "fail";
+console.log(result);
+
+//logic operators
+//&&  va || yoke   !emas
+//&&
+console.log(true && true);
+let hour = 9;
+let minute = 20;
+isOfficeOpen = hour >= 9 && minute >= 0;
+console.log(isOfficeOpen);
+
+// ||
+console.log(true || false);
+// !
+let isOfficeClosed = !isOfficeOpen;
+console.log(isOfficeClosed);
